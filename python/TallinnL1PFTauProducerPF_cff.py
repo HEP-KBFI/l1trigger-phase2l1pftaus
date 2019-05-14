1d89cc91c0a7d9feaec7144bab950c8adba46fe7
Christian Veelken <christian.veelken@cern.ch>
2019-05-14 15:05:16 +0300

    relax dz cut to < 0.4 cm for signal and isolation PFCands

--- python/TallinnL1PFTauProducerPF_cff.py
+++ python/TallinnL1PFTauProducerPF_cff.py
@@ -7,7 +7,7 @@ TallinnL1PFTauProducerPF = cms.EDProducer("TallinnL1PFTauProducer",
   useChargedPFCandSeeds     = cms.bool(True),                          
   min_seedChargedPFCand_pt  = cms.double(5.),
   max_seedChargedPFCand_eta = cms.double(2.1),
-  max_seedChargedPFCand_dz  = cms.double(0.2),
+  max_seedChargedPFCand_dz  = cms.double(0.4),
   usePFJetSeeds             = cms.bool(True),                          
   min_seedPFJet_pt          = cms.double(30.),
   max_seedPFJet_eta         = cms.double(2.1),
@@ -22,25 +22,25 @@ TallinnL1PFTauProducerPF = cms.EDProducer("TallinnL1PFTauProducer",
   max_PFTau_eta             = cms.double(2.1),                                       
   min_leadChargedPFCand_pt  = cms.double(1.),
   max_leadChargedPFCand_eta = cms.double(2.1),
-  max_leadChargedPFCand_dz  = cms.double(0.2),
+  max_leadChargedPFCand_dz  = cms.double(0.4),
   max_chargedIso            = cms.double(1.e+3),
   max_chargedRelIso         = cms.double(1.0),                                       
   deltaR_cleaning           = cms.double(0.4),
   signalQualityCuts = cms.PSet(
     chargedHadron = cms.PSet(
       min_pt = cms.double(0.),
-      max_dz = cms.double(0.2),                                          
+      max_dz = cms.double(0.4),                                          
     ),
     neutralHadron = cms.PSet(
       min_pt = cms.double(0.)
     ),                                        
     muon = cms.PSet(
       min_pt = cms.double(0.),
-      max_dz = cms.double(0.2),                                          
+      max_dz = cms.double(0.4),                                          
     ),
     electron = cms.PSet(
       min_pt = cms.double(0.),
-      max_dz = cms.double(0.2),                                          
+      max_dz = cms.double(0.4),                                          
     ),                                            
     photon = cms.PSet(
       min_pt = cms.double(0.)
@@ -49,18 +49,18 @@ TallinnL1PFTauProducerPF = cms.EDProducer("TallinnL1PFTauProducer",
   isolationQualityCuts = cms.PSet(
     chargedHadron = cms.PSet(
       min_pt = cms.double(0.),
-      max_dz = cms.double(0.2),                                          
+      max_dz = cms.double(0.4),                                          
     ),
     neutralHadron = cms.PSet(
       min_pt = cms.double(0.)
     ),                                        
     muon = cms.PSet(
       min_pt = cms.double(0.),
-      max_dz = cms.double(0.2),                                          
+      max_dz = cms.double(0.4),                                          
     ),
     electron = cms.PSet(
       min_pt = cms.double(0.),
-      max_dz = cms.double(0.2),                                          
+      max_dz = cms.double(0.4),                                          
     ),                                            
     photon = cms.PSet(
       min_pt = cms.double(0.)
